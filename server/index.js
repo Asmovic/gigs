4c15b9a0a6f5aa9e97b64464dbfe087bfa5ca31f
Asmovic <asogba007@gmail.com>
2022-08-11 10:41:07 +0100

    ####ft(setup-babel): setup babel
     - configure babel
    
    [Done]

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
+import express from "express";
+
+// Create global app object
 const app = express();
+
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send("Welcome. Let's create a Gig!'"));
+app.get("/", (req, res) => res.send("Welcome. Let's create a Gig"));
 
-app.listen(PORT, () => console.log(`Server Listening on Port :${port}`));
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: "404 Page Not Found",
+  });
+});
+
+app.listen(process.env.PORT || PORT, () =>
+  console.log(`Server Listening on Port :${PORT}`)
+);
+
+export default app;
