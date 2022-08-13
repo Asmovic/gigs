4b8af6a2ad5e8bc1eb4f4a27e4a093c525d089a6
Asmovic <asogba007@gmail.com>
2022-08-13 18:42:54 +0100

    ch(setup-winston):setup winston

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import morgan from 'morgan';
+import logger from './helpers/logger';
 
 // Create global app object
 const app = express();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Log requests to the console.
+app.use(morgan('dev'));
+
+logger.config();
+
+app.use(morgan(':remote-addr - ":method :url :status ":user-agent"', {
+  stream: logger.stream(),
+  skip: () => !isProduction,
+}));
 
 const PORT = 3000;
 
