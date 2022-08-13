7a15693de96e2e3bc5e7444cf453c91c9124dda6
Asmovic <asogba007@gmail.com>
2022-08-13 17:46:47 +0100

    Resolve eslintrc issues

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
-import express from "express";
+import express from 'express';
 
 // Create global app object
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Welcome. Let's create a Gig"));
-
-app.all("*", (req, res) => {
+app.get('/', (req, res) => res.send("Welcome. Let's create a Gig"));
+app.all('*', (req, res) => {
   res.status(404).json({
     status: 404,
-    error: "404 Page Not Found",
+    error: '404 Page Not Found',
   });
 });
-
-app.listen(process.env.PORT || PORT, () =>
-  console.log(`Server Listening on PORT ${PORT}`)
-);
-
+app.listen(process.env.PORT || PORT, () => console.log(`Server Listening on PORT ${PORT}`));
 export default app;
